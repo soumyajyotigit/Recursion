935b2b79c05bb912cd2398fd09595fe1630eb197
Soumyajyoti Karmakar <ksoumyajyoti@gmail.com>
2023-02-27 19:39:42 +0530

    Create Print name N times.cpp

---
+++ Print name N times.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int i=1;
+void fun(int i,int n){
+    if(i>n){
+        return;
+    }
+    cout<<"Soumya"<<endl;
+    fun(i+1,n);
+}
+int main(){
+    int n;
+    cin>>n;
+    fun(i,n);
+}
